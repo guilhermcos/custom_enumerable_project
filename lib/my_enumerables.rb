57a2b9348436ff7d2e5e15f0241e29f56eede2d8
Guilherme Gouveia <120587680+guilhermcos@users.noreply.github.com>
2023-09-15 14:07:14 -0300

    no comments

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -74,10 +74,6 @@ module Enumerable
   end
 end
 
-# You will first have to define my_each
-# on the Array class. Methods defined in
-# your enumerable module will have access
-# to this method
 class Array
   def my_each
     for i in 0...self.length
